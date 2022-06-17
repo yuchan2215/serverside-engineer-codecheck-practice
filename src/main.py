30bd01d2313ffa2512460616ff06b263abcb60fd
Miyayu <yuchan2215@gmail.com>
2022-06-17 14:21:21 +0900

    [add] １行目のチェック機能を作成した
    [doc] readRowのドキュメントを追加

--- src/main.py
+++ src/main.py
@@ -17,11 +17,48 @@ def get_option() -> Namespace:
     return _argParser.parse_args()
 
 
+def formatCheck(row: list) -> bool:
+    """
+    １行目が想定されているものかどうかチェックします。
+
+    :param row: １行目のリスト。
+    :return: rowが想定されているものと一致しているか
+    """
+    return row == ['create_timestamp', 'player_id', 'score']
+
+
+readFirst: bool = False  # 最初の行を読み込んだか
+
+
+def readRow(row: list):
+    """
+    １行ずつ読み込みます。
+    :param row: 行のデータ
+    """
+    global readFirst
+
+    if not readFirst:
+        # もし最初の行であれば、フォーマットチェックを行う
+        readFirst = True
+        format_correct = formatCheck(row)
+
+        if not format_correct:
+            # 失敗したならエラーを出す。
+            raise FormatError()
+
+        else:
+            # 成功したなら早期リターンをする。
+            return
+
+    # TODO ANY
+
+
 def readCsv():
     with open(fileName) as f:
         reader = csv.reader(f)
         for row in reader:
-            pass  # TODO any
+            # 行の読み込みを行う。
+            readRow(row=row)
 
 
 if __name__ == "__main__":
