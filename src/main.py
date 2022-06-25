64c76feaef1e31eaa9c8807383f966985ec4dba2
Miyayu <yuchan2215@gmail.com>
2022-06-25 20:17:28 +0900

    [add] フィードバックを反映

--- src/main.py
+++ src/main.py
@@ -1,13 +1,59 @@
 import csv
+import itertools
 import sys
 from argparse import ArgumentParser, Namespace
+from typing import Dict, List
 
-from player import Player
+# CSVに対応するタイトル。
+PLAYER_ID_COLUMN = 'player_id'
+SCORE_COLUMN = 'score'
+
+# 出力するプレイヤー数
+REPORT_PLAYERS = 10
 
 
-class FormatError(Exception):
-    """フォーマットが正しく無い時に呼び出されます。"""
-    pass
+class Player:
+    """
+    プレイヤー型。
+    省メモリを実現するために、辞書と併用して利用することを想定してプレイヤーIDは保持していません。
+    """
+    _gameCount: int = 0  # プレイしたゲームの数
+    _sumScore: int = 0  # 合計スコア
+
+    def getAverage(self) -> int:
+        """
+        プレイヤーの平均スコアを計算します。
+        :return: 四捨五入された平均スコアです。
+        """
+        if self._gameCount == 0:
+            # 0除算対策
+            return 0
+
+        # 平均を返す
+        return round(self._sumScore / self._gameCount)
+
+    def addResult(self, score: int):
+        """
+        ゲームの結果を追加します。
+        :param score: 加算するスコア
+        """
+
+        # ゲームのカウント値とスコアを変動させる。
+        self._gameCount += 1
+        self._sumScore += score
+
+
+class RankingObject:
+    """
+    ランキングに使用するオブジェクト。
+    同一スコアのプレイヤーをまとめるために使用します。
+    """
+    score: int
+    playerIds: List[str]
+
+    def __init__(self, score: int, playerIds: List[str]):
+        self.score = score
+        self.playerIds = playerIds
 
 
 def get_option() -> Namespace:
@@ -20,129 +66,106 @@ def get_option() -> Namespace:
     return _argParser.parse_args()
 
 
-def formatCheck(row: list) -> bool:
+def readCsv(fileName: str) -> Dict[str, Player]:
     """
-    １行目が想定されているものかどうかチェックします。
-
-    :param row: １行目のリスト。
-    :return: rowが想定されているものと一致しているか
+    CSVからプレイヤーのデータを読み込みます。
+    :param fileName: 読み込むCSVファイルのパス
+    :return: 読み込み結果をPlayerオブジェクトに入れた辞書型ファイル。
     """
-    return row == ['create_timestamp', 'player_id', 'score']
+    records: Dict[str, Player] = {}
 
-
-readFirst: bool = False  # 最初の行を読み込んだか
-players: dict = {}  # プレイヤー一覧を入れていく辞書
-
-
-def getPlayer(playerId: str) -> Player:
-    """
-    辞書からプレイヤーを探します。無ければ新しく作ります。
-
-    :param playerId: プレイヤーID
-    :return: プレイヤー
-    """
-    if playerId in players:
-        # もし含まれるのであればその値を返す
-        return players.get(playerId)
-    else:
-        # もし含まれないなら新しく生成して返す。
-
-        player = Player()  # 新規プレイヤー
-        players[playerId] = player  # 辞書に追加
-        return player
-
-
-def readRow(row: list):
-    """
-    １行ずつ読み込みます。
-    :param row: 行のデータ
-    """
-    global readFirst
-
-    if not readFirst:
-        # もし最初の行であれば、フォーマットチェックを行う
-        readFirst = True
-        format_correct = formatCheck(row)
-
-        if not format_correct:
-            # 失敗したならエラーを出す。
-            raise FormatError()
-
-        else:
-            # 成功したなら早期リターンをする。
-            return
-
-    # 最初の行で無ければ行の読み込み機能を作っていく。
-    # 日付は扱わないのでプログラム内で取り扱わない。
-    name = str(row[1])
-    score = int(row[2])
-
-    # プレイヤーを辞書から取得し、結果を追加する。
-    getPlayer(name).addResult(score)
-
-
-def readCsv():
-    """
-    CSVを読み込みます。
-    """
     with open(fileName, 'r') as f:
-        reader = csv.reader(f)
+        reader = csv.DictReader(f, lineterminator="\n")
         for row in reader:
-            # 行の読み込みを行う。
-            readRow(row=row)
+            # CSVの内容を読み込む
+            player_id = row[PLAYER_ID_COLUMN]
+            score = int(row[SCORE_COLUMN])
+            # レコードがないなら作成する
+            if player_id not in records:
+                records[player_id] = Player()
+            # レコードの内容を書き換える
+            record = records[player_id]
+            record.addResult(score)
+    return records
 
 
-def dicSort() -> list:
+def mean_by_player(records: Dict[str, Player]) -> Dict[str, int]:
     """
-    プレイヤーの辞書を平均得点順でソートします。
-    :return: ソートされたリストです。
+    プレイヤーの平均点を求めた辞書を作成します。
+    算出方法はPlayerクラスに依存します。
+    :param records: プレイヤー一覧のレコード
+    :return: プレイヤーごとの平均点の辞書
     """
-    global players
+    means: Dict[str, int] = {}
 
-    return sorted(players.items(), key=lambda p: (
-        p[1].getAverage() * -1,  # 点数のソート。名前のソートもするため、Reverseを使わずに-1をかける。
-        p[0]  # 名前でソートする。
-    ))
+    for playerId, record in records.items():
+        means[playerId] = record.getAverage()
+
+    return means
 
 
-def output(playersList: list):
+def make_ranking(means: Dict[str, int]) -> Dict[int, RankingObject]:
     """
-    結果を出力します
-    :param playersList: プレイヤーのリスト
+    平均点によるランキングを作成します。
+    同一点数のものは、同一順位として扱います。
+    :param means: プレイヤーごとの平均点の辞書
+    :return: 順位ごとのRankingObjectの辞書
     """
-    count = 0  # 順位重複関係なく増える値
-    rank = 0  # 順位重複を考慮した値
-    cacheScore = None  # 順位重複を考慮するためのキャッシュ
+    # 点数降順でソートする。
+    sorted_means = sorted(means.items(), reverse=True, key=lambda x: x[1])
+    # 点数ごとにまとめる。
+    grouped_means = itertools.groupby(sorted_means, lambda x: x[1])
 
+    ranking_data: Dict[int, RankingObject] = {}
+    rank = 1
+
+    for score, player_id_and_score in grouped_means:
+        # groupByしたアイテムの中から、player_idを引っ張り出してくる。
+        player_ids = list(map(lambda x: x[0], list(player_id_and_score)))
+
+        # 反映させる。
+        ranking_data[rank] = RankingObject(score, player_ids)
+        rank += len(player_ids)
+
+    return ranking_data
+
+
+def output(rankDict: Dict[int, RankingObject], report_players: int = REPORT_PLAYERS):
+    """
+    ランキングを出力します。
+    同じ順位のプレイヤーが複数いる場合は、プレイヤー名昇順で出力します。
+    :param rankDict: ランキング形式の辞書
+    :param report_players: 発表するプレイヤー数
+    """
     print("rank,player_id,mean_score")
-    for i in playersList:
-        playerId: str = i[0]
-        player: Player = i[1]
+    printed = 0
+    for (rank, rankingObj) in rankDict.items():
+        rank: int
+        rankingObj: RankingObject
 
-        avg = player.getAverage()
-        count += 1
+        # 出力した件数が出力する順位以上であれば抜ける。
+        if report_players <= printed:
+            break
 
-        # 点数変動があるなら
-        if cacheScore != avg:
-            if count > 10:
-                # 生の順位が11を超えているなら抜ける
-                break
+        # プレイヤー名昇順で出力する。
+        sorted_player_ids = sorted(rankingObj.playerIds)
+        for player_id in sorted_player_ids:
+            print(f"{rank},{player_id},{rankingObj.score}")
 
-            rank = count  # ランク更新する
-            cacheScore = avg  # キャッシュを更新する
-
-        print(f'{rank},{playerId},{avg}')
+        # 印刷件数を変える。
+        printed += len(sorted_player_ids)
 
 
-if __name__ == "__main__":
+def main():
     # コマンドライン引数の読み取り
     args = get_option()
     fileName = str(args.fileName)
 
     # CSVを読み込む（エラーがあれば出力する。）
     error = True
+    records: Dict[str, Player] = {}
     try:
-        readCsv()
+        records = readCsv(fileName)
     except FileNotFoundError:
         print(f'"{fileName}"というファイルは見つかりませんでした。')
     except IsADirectoryError:
@@ -151,8 +174,6 @@ if __name__ == "__main__":
         print('権限がありません\n', e)
     except ValueError as e:
         print('CSVの値に想定外のものがありました\n', e)
-    except FormatError:
-        print('ファイルのフォーマットが正しくありません。')
     except Exception as e:
         print('CSVの読み込みに失敗しました\n', e)
     else:
@@ -162,5 +183,10 @@ if __name__ == "__main__":
         # エラーがあったのであればプログラムを終了する。
         sys.exit(1)
 
-    sortedList = dicSort()  # ソートする
-    output(sortedList)  # 出力する
+    player_mean_scores = mean_by_player(records)
+    ranking_dict = make_ranking(player_mean_scores)
+    output(ranking_dict)
+
+
+if __name__ == "__main__":
+    main()
